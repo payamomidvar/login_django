416424c26de434d2cf18273764ca3bbd57edcba2
payamomidvar <payamomidvar9273@gmail.com>
2023-08-01 09:30:20 -0700

    User model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,82 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core import validators
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserManger(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username,
+                          email=email,
+                          is_staff=is_staff,
+                          is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+
+        if not extra_fields.get('no_password'):
+            user.set_password(password)
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        return self._create_user(username, email, password, True, True, **extra_fields)
+
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        if username is None:
+            if email:
+                username = email.split('@', 1)[0]
+            while User.objects.filter(username=username).exists():
+                username += str(random.randint(10, 99))
+            return self._create_user(username, email, password, False, False, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=32, unique=True,
+                                help_text=_(
+                                    'Required. 30 characters or fewer starting with a letter.'
+                                    ' Letters, digits and underscore only.'),
+                                validators=[
+                                    validators.RegexValidator(r'^[a-zA-Z][a-zA-Z0-9_\.]+$',
+                                                              _('Enter a valid username starting with a-z. '
+                                                                'This value may contain only letters, numbers '
+                                                                'and underscore characters.'),
+                                                              'invalid'),
+                                ],
+                                error_messages={
+                                    'unique': _('A user with that username already exists.')
+                                }
+                                )
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(_('Staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin site.')
+                                   )
+    is_active = models.BooleanField(_('active'), default=True, help_text=_(
+        'Designates whether this user should be treated as active. '
+        'Unselect this instead of deleting accounts.'
+    ))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    last_seen = models.DateTimeField(_('last seen date'), null=True)
+
+    objects = UserManger()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def save(self, *args, **kwargs):
+        if self.email is not None and self.email.strip() == '':
+            self.email = None
+        super().save(*args, **kwargs)
